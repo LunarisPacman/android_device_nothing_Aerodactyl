b77053a7bc0e0665ffc124e0c5427a9aa1272168
TwistedVision518 <brawlclicks58@gmail.com>
2026-01-17 12:34:22 +0000

    Aerodactyl: Add Lunaris configurations

--- lineage_Pacman.mk
+++ lineage_Pacman.mk
@@ -15,6 +15,36 @@ PRODUCT_MANUFACTURER := Nothing
 PRODUCT_MODEL := A142
 PRODUCT_NAME := lineage_Pacman
 
+#GMS
+WITH_GMS := true
+TARGET_USES_OMNI_GAPPS :=true
+
+#blur
+WITH_BLUR := true
+
+#Animation
+PERF_ANIM_OVERRIDE := true
+
+LUNARIS_BUILD_TYPE := UNOFFICIAL
+# Maintainer Name
+LUNARIS_MAINTAINER := TwistedVis
+# Whether the device supports Fingerprint On Display
+TARGET_HAS_UDFPS := true
+
+TARGET_SHIPS_GOOGLE_DIALER := true
+USE_MOTO_CALCULATOR := true
+PERF_ANIM_OVERRIDE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Lunaris AboutPhone
+PRODUCT_SYSTEM_PROPERTIES  += \
+	ro.product.marketname=Nothing Phone 2a\
+	ro.lunaris.soc=Mediatek Dimensity 7200 Pro \
+	ro.lunaris.battery=5000mAh \
+	ro.lunaris.display=2400x1084,120Hz \
+	ro.lunaris.camera=50MP+50MP
+
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
