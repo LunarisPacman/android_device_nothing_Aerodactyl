e3d19d9df0a7c87898becf54498b425ef640e028
TwistedVision518 <brawlclicks58@gmail.com>
2026-03-29 12:52:05 +0530

    Aerodactyl: Fix product parsing for lunch and dumpvars
    Define PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE before the first product-side conditional, remove the duplicated virtual A/B block, and keep BoardConfig aligned so lunch and dumpvars see a stable default.
    
    (cherry picked from commit dfa4c8e1478ae707a8763aaf54a50b55a47fbcc2)

--- device-common.mk
+++ device-common.mk
@@ -7,6 +7,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Define this before the first conditional so lunch/dumpvars has a stable default
+# while product makefiles are being parsed.
+PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE ?= ext4
+
 # A/B
 ifeq ($(WITH_GMS),true)
 PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE ?= erofs
@@ -21,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.m
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
 endif
+
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
