99e11c4d6c20c829c02fcb801178300fedf075ea
TwistedVision518 <brawlclicks58@gmail.com>
2026-04-02 13:47:28 +0000

    Aerodactyl: Wire ViPER4AndroidFX support
    Inherit the ViPER4AndroidFX product config and expose the vendor audio effect library and UUID so the effect can be loaded correctly on this device.

--- device-common.mk
+++ device-common.mk
@@ -6,6 +6,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Define this before the first conditional so lunch/dumpvars has a stable default
 # while product makefiles are being parsed.
